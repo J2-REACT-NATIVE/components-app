d334c661dae4632b7815021173037820cc2af063
Juan Collantes <juan@collantes.ec>
2026-05-27 23:59:01 -0500

    fadeIn fadeOut

--- src/app/animation-101/index.tsx
+++ src/app/animation-101/index.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native';
+import ThemedButton from '../../presentation/shared/ThemedButton';
 
 const Animation101Screen = () => {
   return (
     <View>
       <Text>Animation101Screen</Text>
+      <ThemedButton className="my-5" onPress={()=>console.log("Fade In") }>FadeIn</ThemedButton>
+      <ThemedButton className="my-5" onPress={()=>console.log("Fade out") }>FadeIn</ThemedButton>
     </View>
   );
 };
